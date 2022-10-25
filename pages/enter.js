7ef222548daab60744d48298cadbe6255a52ede7
Duncan <duncantheinvictus@gmail.com>
2022-10-25 21:19:16 +0800

    push

--- pages/enter.js
+++ pages/enter.js
@@ -1,10 +1,14 @@
 import { auth, googleAuthProvider } from "../lib/firebase";
 import { useContext } from "react";
 import { UserContext } from "../lib/context";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function Enter(props) {
-  const user = null;
-  const username = null;
+  const { user, username } = useContext(UserContext);
+  // const username = null;
+  // const [user] = useAuthState(auth);
+  console.log(user);
+  console.log(username);
 
   // const { user, username } = useContext(UserContext);
   // 1. user signed out <SignInButton />
@@ -49,4 +53,37 @@ function SignOutButton() {
   return <button onClick={() => auth.signOut()}>Sign Out</button>;
 }
 
-function UsernameForm() {}
+function UsernameForm() {
+  const [formValue, setFormValue] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { user, username } = useContext(Usercontext);
+  return (
+    !username && (
+      <section>
+        <h3>Choose Username</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            name="username"
+            placeholder="username"
+            value={formValue}
+            onChange={onChange}
+          />
+
+          <button type="submit" className="btn-green" disabled={!isValid}>
+            Choose
+          </button>
+          <h3>Debug State</h3>
+          <div>
+            Username: {formValue}
+            <br />
+            Loading: {loading.toString()}
+            <br />
+            Username Valid: {isValid.toString()}
+          </div>
+        </form>
+      </section>
+    )
+  );
+}
