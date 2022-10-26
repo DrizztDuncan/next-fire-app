a621567b7dee5fc25556277a54f24d65ba1d711e
Duncan <duncantheinvictus@gmail.com>
2022-10-26 14:55:14 +0800

    Update enter.js

--- pages/enter.js
+++ pages/enter.js
@@ -59,6 +59,41 @@ function UsernameForm() {
   const [loading, setLoading] = useState(false);
 
   const { user, username } = useContext(Usercontext);
+
+  useEffect(() => {
+    checkUsername(formValue);
+  }, [formValue]);
+
+  const onChange = (e) => {
+    //force from value typed in form to match correct format
+    const val = e.target.value.toLowerCase();
+    const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+    // only set form value if length is < 3 or it passing regex
+    if (val.length < 3) {
+      setFormValue(val);
+      setLoading(false);
+      setIsValid(false);
+    }
+
+    if (re.test(val)) {
+      setFormValue(val);
+      setLoading(true);
+      setIsValid(false);
+    }
+  };
+
+  // hit the database for username match after each debounced change
+  // useCallback is require for debounce to work
+  const checkUsername = async (username) => {
+    if (username.length >= 3) {
+      const ref = firestore.doc(`usernames/${username}`);
+      const { exists } = await ref.get();
+      console.log("Firestore read executed!");
+      setIsValid(!exists);
+      setLoading(false);
+    }
+  };
   return (
     !username && (
       <section>
