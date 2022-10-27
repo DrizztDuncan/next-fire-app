6b6760d88c7b4ed156e0dc038c7f8e45c281da93
DrizztDuncan <duncantheinvictus@gmail.com>
2022-10-28 00:23:40 +0800

    push

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,6 +1,14 @@
+import UserProfile from "../../components/UserProfile";
+import PostFeed from "../../components/PostFeed";
+
 //any route we index to beyond the route url will go to the user name page
 // will not conflict with the enter admin route
 // cuz Next.js will prefer the route already existed in a static routes than a dynamic routes
 export default function UserProfilePage({}) {
-  return <main></main>;
+  return (
+    <main>
+      <UserProfile user={user} />
+      <PostFeed user={posts} />
+    </main>
+  );
 }
