cefaee8ff3bb73ebdf0ba0982950075e9598b687
DrizztDuncan <duncantheinvictus@gmail.com>
2022-10-31 22:30:44 +0800

    SSR home page feed
    Not done yet

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,10 +1,33 @@
+import { getUserWithUsername } from "../../lib/firebase";
 import UserProfile from "../../components/UserProfile";
 import PostFeed from "../../components/PostFeed";
+
+export async function getServerSideProps({ query }) {
+  const { username } = query;
+  const userDoc = await getUserWithUsername(username);
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+      .collection("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .limit(5);
+
+    posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+  return {
+    props: { user, posts }, // will be passed to the page component as props
+  };
+}
 
 //any route we index to beyond the route url will go to the user name page
 // will not conflict with the enter admin route
 // cuz Next.js will prefer the route already existed in a static routes than a dynamic routes
-export default function UserProfilePage({}) {
+export default function UserProfilePage({ user, posts }) {
   return (
     <main>
       <UserProfile user={user} />
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,26 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import toast from "react-hot-toast";
 
+import PostFeed from "../components/PostFeed";
 import Loader from "../components/Loader";
+import { firestore, fromMillis, postToJSON } from "../lib/firebase";
+
+import { useState } from "react";
+
+// Max post to query per page
+const LIMIT = 1;
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup("posts")
+    .where("published", "==", true)
+    .orderBy("createdAt", "desc")
+    .limit(LIMIT);
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return {};
+}
 
 export default function Home() {
   return (
